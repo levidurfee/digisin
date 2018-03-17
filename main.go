40c6e5851b7fb8cd6f6e96e68de2e9d3dff325bf
Levi Durfee <levi.durfee@gmail.com>
2018-03-17 18:39:08 -0400

    format code

--- main.go
+++ main.go
@@ -3,12 +3,12 @@ package main
 import (
 	"crypto/sha512"
 	"fmt"
+	"math/rand"
 	"os"
 	"strconv"
+	"sync"
+	"time"
 	"unicode/utf8"
-    "math/rand"
-    "time"
-    "sync"
 )
 
 func main() {
@@ -16,37 +16,36 @@ func main() {
 	args := os.Args[1:]
 	target := args[1]
 	inputHash := args[0]
-    var wg sync.WaitGroup
-    wg.Add(4)
-    for i := 0; i < 4; i++ {
-        go func(i int) {
-            defer wg.Done()
-            Mine(inputHash, target, i)
-        }(i)
-    }
-    wg.Wait()
+	var wg sync.WaitGroup
+	wg.Add(4)
+	for i := 0; i < 4; i++ {
+		go func(i int) {
+			defer wg.Done()
+			Mine(inputHash, target, i)
+		}(i)
+	}
+	wg.Wait()
 }
 
 func Mine(input string, target string, x int) bool {
-    var i uint64
-
+	var i uint64
 
 	for i = 0; i < 18446744073709551615; i++ {
-        r := rand.New(rand.NewSource(time.Now().UnixNano()))
-        nonce := r.Uint64()
-        hash := GetHash(input + "." + S(nonce))
-        fmt.Printf("Thread %d: %s\tHash: %s...\n", x, S(nonce), hash[0:32])
+		r := rand.New(rand.NewSource(time.Now().UnixNano()))
+		nonce := r.Uint64()
+		hash := GetHash(input + "." + S(nonce))
+		fmt.Printf("Thread %d: %s\tHash: %s...\n", x, S(nonce), hash[0:32])
 
 		if CheckHash(hash, target) {
-            fmt.Println("---------Found----------")
-            fmt.Println(input + "." + S(nonce))
+			fmt.Println("---------Found----------")
+			fmt.Println(input + "." + S(nonce))
 			fmt.Println(hash)
 
-            return true
+			return true
 		}
 	}
 
-    return false
+	return false
 }
 
 func CheckHash(hash string, target string) bool {
