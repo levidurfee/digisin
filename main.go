bb2a88a30aa26969f9301005c44f4d0dfe01a889
Levi Durfee <levi.durfee@gmail.com>
2018-03-17 11:28:44 -0400

    initial commit

---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"crypto/sha512"
+	"fmt"
+	"os"
+	"strconv"
+	"unicode/utf8"
+)
+
+func main() {
+	fmt.Println("Starting")
+	args := os.Args[1:]
+	target := args[1]
+	inputHash := args[0]
+
+	var i uint64
+
+	for i = 0; i < 18446744073709551615; i++ {
+		hash := GetHash(inputHash + "." + S(i))
+		fmt.Println(hash)
+
+		if CheckHash(hash, target) {
+			fmt.Println("Found: " + S(i))
+			os.Exit(1)
+		}
+	}
+}
+
+func CheckHash(hash string, target string) bool {
+	if TrimHash(hash, utf8.RuneCountInString(target)) == target {
+		return true
+	}
+
+	return false
+}
+
+func TrimHash(hash string, len int) string {
+	start := 128 - len
+	trimmed := hash[start:128]
+	//fmt.Println(trimmed)
+
+	return trimmed
+}
+
+func S(i uint64) string {
+	return strconv.FormatUint(i, 10)
+}
+
+func GetHash(s string) string {
+	hasher := sha512.New()
+	hasher.Write([]byte(s))
+	result := hasher.Sum(nil)
+
+	hash := fmt.Sprintf("%x", result)
+
+	return hash
+}
